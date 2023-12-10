f7b620993b7684fe46b581a55fc6fa028643fde5
DenysWinPC <denkochev78@gmail.com>
2023-12-10 16:34:38 +0100

    feat: encrypt decrypt example

--- main.go
+++ main.go
@@ -21,4 +21,11 @@ func main() {
 
 	// isSignatureValid := signature.Verify([]byte(message), pair.GetPublic())
 	// fmt.Println(isSignatureValid)
+
+	alice_keyPair := elgamal.KeyPair{}
+	alice_keyPair.GenerateKeyPair()
+
+	make_cypher := elgamal.Encrypt([]byte(message), alice_keyPair.GetPublic())
+	make_cypher.Decrypt(alice_keyPair.GetPrivate())
+
 }
