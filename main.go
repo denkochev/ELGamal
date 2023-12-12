78a8b70dff4c546e4800c4789e01e3603bb09ab8
DenysWinPC <denkochev78@gmail.com>
2023-12-12 14:31:04 +0100

    feat: add example with data > 4096 bits

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	message := "Modern programming langugages are fast"
+	message := "Modern programming languages are fast. Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. Modern programming languages are fast. "
+	fmt.Println(len([]byte(message)) * 8)
 	signature, keypair := elgamal.UnstableSignature(message)
 
 	fmt.Println(signature.Verify([]byte(message), keypair.GetPublic()))
