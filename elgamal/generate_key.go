36f7fbe386a534e1eac8cf629749748a620bce06
DenysWinPC <denkochev78@gmail.com>
2023-12-12 15:51:22 +0100

    refactor: memory safe key pair generator

--- elgamal/generate_key.go
+++ elgamal/generate_key.go
@@ -9,14 +9,15 @@ type KeyPair struct {
 	public  *big.Int
 }
 
-func (pair *KeyPair) GenerateKeyPair() {
+func GenerateKeyPair() *KeyPair {
 	a := PrivateGenerator() // random in [1, p-1] | NUMBER HAS TO BE COPRIME WITH P-1 !
 
 	b := big.NewInt(0)
 	b.Exp(Params().g, a, Params().p) // g^a mod p
-
-	pair.private = a
-	pair.public = b
+	return &KeyPair{
+		private: a,
+		public:  b,
+	}
 }
 
 func (pair *KeyPair) GetPrivate() *big.Int {
